2823340813e85cc50ed4af38a888d5bb0aa70951
JamesAUre <jamesandrewure@gmail.com>
2020-03-06 01:02:02 +1100

    Added another function to handle player turns

--- TicTacToe/src/Main.java
+++ TicTacToe/src/Main.java
@@ -10,33 +10,20 @@ public class Main {
 	//Game backbone function
 	static void game(){
 		Board board = new Board();
-		Scanner user_piece = new Scanner(System.in);
-		int x=0;
-		int y=0;
+		Location insert_loc = new Location();
 		
 		//game loop for turns
 		while (!board.checkGameOver()) {
 			if(board.getPlayer()) {
-				do {
-					//player1 interface
-					System.out.println("O turn");
-					System.out.print("enter x: ");
-					x = user_piece.nextInt() - 1;
-					System.out.print("enter y: ");
-					y = user_piece.nextInt() - 1;
-				} while (!board.validMove(x, y));
-				board.insertPiece(x, y, 0);
+				System.out.println("O turn");
+				insert_loc = getMove(board);
+				board.insertPiece(insert_loc.getX(), insert_loc.getY(), 0);
+	
 			}
 			else {
-				do {
-					//player2 interface
-					System.out.println("X turn");
-					System.out.print("enter x: ");
-					x = user_piece.nextInt() - 1;
-					System.out.print("enter y: ");
-					y = user_piece.nextInt() - 1;
-				} while (!board.validMove(x, y));
-				board.insertPiece(x, y, 1);
+				System.out.println("X turn");
+				insert_loc = getMove(board);
+				board.insertPiece(insert_loc.getX(), insert_loc.getY(), 1);
 			}
 			
 			//both players interface
@@ -47,5 +34,27 @@ public class Main {
 		//exit interface
 		System.out.println("GAME FINISHED!");
 		System.out.println("Winner is: " + board.checkWinner());
+	}
+	
+	//Handles each turn of the user
+	static Location getMove(Board currentboard) {
+		
+		//all the data I need
+		Location loc = new Location();
+		Scanner user_piece = new Scanner(System.in);
+		int x=0;
+		int y=0;
+		
+		//getting x and y coordinate until coordinate is empty
+		do {
+			System.out.print("enter x: ");
+			x = user_piece.nextInt() - 1;
+			System.out.print("enter y: ");
+			y = user_piece.nextInt() - 1;
+		} while(!currentboard.validMove(x, y));
+		
+		//storing it as a location and returning
+		loc.set(x, y);
+		return loc;
 	}
 }
