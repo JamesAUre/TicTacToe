a2dc9532b956fd1492319c5483e1659b508771c8
JamesAUre <jamesandrewure@gmail.com>
2020-03-06 01:01:47 +1100

    Added a location class to handle coordinates of x and y

---
+++ TicTacToe/src/Location.java
@@ -0,0 +1,20 @@
+
+public class Location {
+	int x;
+	int y;
+	
+	Location(){
+		x = 0;
+		y = 0;
+	}
+	void set(int x_in, int y_in) {
+		x = x_in;
+		y = y_in;
+	}
+	int getX() {
+		return x;
+	}
+	int getY() {
+		return y;
+	}
+}
